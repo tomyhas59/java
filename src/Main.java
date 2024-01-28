924540be33884e7fbc71c36bf45fae6c0c4af0ef
yh9035926 <yh9035926@naver.com>
2024-01-29 05:52:16 +0900

    final

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("자바 프로그래밍"); //sout
+
+        double pi = 3.14; //실수
+        int age = 20; //정수
+        boolean isMarried = true;
+        char sex = 'M'; //한 글자
+        String name = "홍길동"; //문자
+        System.out.println(pi + age);
+
+        final int NUM = 1;
+        System.out.printf("1" + NUM);
+    }
+}
+
