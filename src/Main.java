79a2b9ed280b9a351a36eeb04894762836fa79ef
yh9035926 <yh9035926@naver.com>
2024-01-31 05:22:31 +0900

    byte

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,19 @@ public class Main {
 
         double pi = 3.14; //실수
         int age = 20; //정수
+        byte    _1b=1; //-127~127
+        short   _2b=123;
+        long    _8b=4213213123123L; //L 붙이면 큰 수 가능
         boolean isMarried = true;
         char sex = 'M'; //한 글자
         String name = "홍길동"; //문자
-        System.out.println(pi + age);
 
-        final int NUM = 1;
-        System.out.println("1" + NUM);
+        final int NUM = 1;  //const
+        System.out.println(_8b);
 
+//명시적 형변환
+        _1b= (byte) _2b;
+        System.out.println(_1b);
 
     }
 }
