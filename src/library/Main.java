0fb02a2465b07177d26400804d94649514684eb7
yh9035926 <yh9035926@naver.com>
2024-02-20 02:51:19 +0900

    LocalDateTime

--- src/library/Main.java
+++ src/library/Main.java
@@ -2,7 +2,8 @@ package library;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.util.Arrays;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 import java.util.Random;
 import java.util.StringJoiner;
 
@@ -56,8 +57,46 @@ public class Main {
                 .insert(2,"ABC")
                 .replace(2,4,"OneTwo")
                 .reverse()
-                .toString();
+                .toString(); //toString()을 해야 반환됨
 
+        //날짜와 시간--------------------------------------------
+        LocalDate today=LocalDate.now(); //현재 년도-월-일
+        LocalTime thisTime=LocalTime.now(); //현재 시간:분:초
+        LocalDateTime now=LocalDateTime.now();//현재 년도-월-일, T 시간:분:초
 
+        DateTimeFormatter formatter=DateTimeFormatter.ofPattern("yyyy년 MM월 dd일, HH시 mm분 ss초");
+        System.out.println(now.format(formatter));
+
+        LocalDateTime lunchTime=LocalDateTime.of(2024,2,25,18, 0);
+
+        var newTime= lunchTime.minusDays(1); //lunchTime은 바뀌지 않고 새로운 newTime 생성
+        System.out.println(newTime);
+
+        ZonedDateTime zonedDateTime=ZonedDateTime.now();
+        var here=zonedDateTime.getZone();
+        System.out.println(here); //Asia/Seoul
+
+        LocalDate dayE=LocalDate.of(1993,2,25);
+        LocalDate dayY=LocalDate.of(1991,1,31);
+        //문자열에서 시간 인스턴스로------------------------------------------
+        var dinnerTime="2024/02/23 18:30:00";
+        DateTimeFormatter formatterD=DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        var newDinnerTime=LocalDateTime.parse(dinnerTime,formatterD);
+        //날짜 차이 계산----------------------------------------------------
+        var day=Period.between(dayY,dayE);
+        int[] days={
+                day.getYears(),
+                day.getDays(),
+                day.getMonths(),
+              };
+
+        LocalDateTime randomTime=LocalDateTime.of(2024,3,31,12,30);
+        Duration duDay=Duration.between(randomTime, now);
+        long[] duDays={
+                duDay.toDays(),
+                duDay.toHours(),
+                duDay.toMinutes(),
+                duDay.toSeconds(),
+        };
     }
 }
